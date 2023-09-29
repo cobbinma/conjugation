0a58210384e0c8c76a60f79c215ae0d066972d8d
matthew.cobbing <cobbinma@gmail.com>
2023-09-29 10:34:07 +0100

    fix: loading or content

--- conjugation-ui/src/routes/+page.svelte
+++ conjugation-ui/src/routes/+page.svelte
@@ -14,40 +14,41 @@
 </svelte:head>
 
 <div class="flex justify-center w-full max-w-xs">
-	<section>
-		<form method="GET" action="/verb">
-			{#if infinitive}
-				<input type="hidden" name="infinitive" value={infinitive} />
-			{/if}
-			{#if selected && selected !== UNSELECTED_TENSE}
-				<input type="hidden" name="tense" value={selected} />
-			{/if}
-			<div>
-				<input
-					placeholder="Type your verb"
-					class="input select-primary w-full"
-					bind:value={infinitive}
-				/>
-				<select class="select select-primary w-full mt-1" bind:value={selected}>
-					<option disabled selected>{UNSELECTED_TENSE}</option>
-					{#each Object.keys(Tense) as tense}
-						<option value={tense}>
-							{tense.replace(/([A-Z][a-z])/g, ' $1')}
-						</option>
-					{/each}
-				</select>
-				{#if infinitive !== '' && selected !== UNSELECTED}
-					<div><button class="btn btn-primary mt-1">Go</button></div>
-				{/if}
-			</div>
-		</form>
-		<div class="mt-5">
-			<form method="GET" action="/verb">
-				<button class="btn btn-secondary">Random</button>
-			</form>
-		</div>
-	</section>
 	{#if $navigating}
 		<span class="loading loading-ball loading-lg" />
+	{:else}
+		<section>
+			<form method="GET" action="/verb">
+				{#if infinitive}
+					<input type="hidden" name="infinitive" value={infinitive} />
+				{/if}
+				{#if selected && selected !== UNSELECTED_TENSE}
+					<input type="hidden" name="tense" value={selected} />
+				{/if}
+				<div>
+					<input
+						placeholder="Type your verb"
+						class="input select-primary w-full"
+						bind:value={infinitive}
+					/>
+					<select class="select select-primary w-full mt-1" bind:value={selected}>
+						<option disabled selected>{UNSELECTED_TENSE}</option>
+						{#each Object.keys(Tense) as tense}
+							<option value={tense}>
+								{tense.replace(/([A-Z][a-z])/g, ' $1')}
+							</option>
+						{/each}
+					</select>
+					{#if infinitive !== '' && selected !== UNSELECTED}
+						<div><button class="btn btn-primary mt-1">Go</button></div>
+					{/if}
+				</div>
+			</form>
+			<div class="mt-5">
+				<form method="GET" action="/verb">
+					<button class="btn btn-secondary">Random</button>
+				</form>
+			</div>
+		</section>
 	{/if}
 </div>
